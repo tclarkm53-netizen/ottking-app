08ef4d078665e87e078a3bdbe9d0fb32bc8334d0
tclarkm53-netizen <tclarkm53@gmail.com>
2026-05-28 13:47:45 +0600

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/ottking/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/ottking/app/MainActivity.kt
@@ -6,5 +6,8 @@ import io.flutter.embedding.engine.FlutterEngine
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        window.addFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+        )
     }
 }
