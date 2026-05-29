abbd824ba1bf5be03bfa44c7058acc10f2575741
tclarkm53-netizen <tclarkm53@gmail.com>
2026-05-29 09:46:40 +0600

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/ottking/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/ottking/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ottking.app
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import android.view.WindowManager // 👈 এই ইমপোর্ট লাইনটি যোগ করা হয়েছে
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
